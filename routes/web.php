48be01de457c628f7af51073132b0473df134c62
Harry <hareom284@gmail.com>
2025-11-18 23:15:30 +0700

    todo fixed logout and middleware issue

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\PermissionController;
+use Illuminate\Support\Facades\Auth;
 
 
 /*
@@ -26,21 +27,30 @@ Route::get('/', function () {
 Route::get('/',[App\Http\Controllers\TemplateController::class, 'index'])->name('home');
 Route::get('/about',[App\Http\Controllers\TemplateController::class, 'about'])->name('about');
 Route::get('/contact',[App\Http\Controllers\TemplateController::class, 'contact'])->name('contact');
-
-
-//category routes
-Route::resource('categories',CategoryController::class);
-
-
-Route::get('/',[App\Http\Controllers\AdminTemplateController::class, 'index'])->name('index');
-Route::get('/layoutstat',[App\Http\Controllers\AdminTemplateController::class, 'layoutstat'])->name('layoutstat');
-Route::get('/layoutsidenavs',[App\Http\Controllers\AdminTemplateController::class, 'layoutsidenav'])->name('layoutsidenavs');
 Route::get('/login',[App\Http\Controllers\AdminTemplateController::class, 'login'])->name('login');
 Route::get('/register',[App\Http\Controllers\AdminTemplateController::class, 'register'])->name('register');
 Route::get('/password',[App\Http\Controllers\AdminTemplateController::class, 'password'])->name('password');
 Route::get('/errors',[App\Http\Controllers\AdminTemplateController::class, 'error'])->name('errors');
 Route::get('/charts',[App\Http\Controllers\AdminTemplateController::class, 'chart'])->name('charts');
 Route::get('/tables',[App\Http\Controllers\AdminTemplateController::class, 'table'])->name('tables');
+
+
+
+
+
+
+
+
+
+
+Route::get('/layoutstat', [App\Http\Controllers\AdminTemplateController::class, 'layoutstat'])->name('layoutstat');
+Route::get('/layoutsidenavs', [App\Http\Controllers\AdminTemplateController::class, 'layoutsidenav'])->name('layoutsidenavs');
+Route::get('/login', [App\Http\Controllers\AdminTemplateController::class, 'login'])->name('login');
+Route::get('/register', [App\Http\Controllers\AdminTemplateController::class, 'register'])->name('register');
+Route::get('/password', [App\Http\Controllers\AdminTemplateController::class, 'password'])->name('password');
+Route::get('/errors', [App\Http\Controllers\AdminTemplateController::class, 'error'])->name('errors');
+Route::get('/charts', [App\Http\Controllers\AdminTemplateController::class, 'chart'])->name('charts');
+Route::get('/tables', [App\Http\Controllers\AdminTemplateController::class, 'table'])->name('tables');
 
 
 Auth::routes();
@@ -51,19 +61,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::group(['middleware' => ['auth']], function () {
 
+    //only able to access if user login
+    Route::get('/', [App\Http\Controllers\AdminTemplateController::class, 'index'])->name('index');
     // Users CRUD
-    Route::group(['middleware' => ['permission:user_create|user_edit|user_delete|user_view']], function () {
-        Route::resource('users', UserController::class);
-    });
+    Route::resource('users', UserController::class);
 
-    // Permissions CRUD->for admin 
-    Route::group(['middleware' => ['permission:permission_create|permission_edit|permission_delete|permission_view']], function () {
-        Route::resource('permissions', PermissionController::class);
-    });
+    // Permissions CRUD->for admin
+    Route::resource('permissions', PermissionController::class);
 
-    // Roles CRUD->for admin    
-    Route::group(['middleware' => ['permission:role_create|role_edit|role_delete|role_view']], function () {
-        Route::resource('roles', RoleController::class);
-    });
-
+    // Roles CRUD->for admin
+    Route::resource('roles', RoleController::class);
 });
